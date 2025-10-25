6626e21d179f25bb81124bbe9758bee1f41f4bc4
Deniz <denizaskar05@gmail.com>
2025-10-25 21:30:03 +0300

    feat: improve authentication flow and update query handling

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,19 +1,3 @@
-// import {defineQuery} from "next-sanity"
-
-// export const STARTUPS_QUERY=
-// defineQuery(`*[_type =="startup" && defined(slug.current)] && !defined($search) || title match $search || category match $search || author -> name $search] || order(_createdAt desc){
-    
-//     _id,
-//     title,
-//     slug,
-//     _createdAt,
-//     author->{_id , name,image, bio},
-//     views,
-//     description,
-//     category, 
-//     image
-//     }`)
-
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(`*[
@@ -52,3 +36,14 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $
 
 export const STARTUP_VİEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
     _id, views}`)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+    *[ _type== "author" && id == $id][0]{
+        _id,
+        id,
+        name,
+        username,
+        email,
+        image,
+        bio 
+    }`);
